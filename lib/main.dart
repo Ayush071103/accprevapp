aa0f7661572da288f6fab04fab145453782962f5
Ayush071103 <ayushbodana711@gmail.com>
2024-01-27 11:36:32 +0530

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,145 +1,70 @@
-import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:nice_intro/intro_screen.dart';
+import 'package:nice_intro/intro_screens.dart';
+
 
 void main() => runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: Walkthrough(),
     )
 );
 
-class HomePage extends StatelessWidget {
+class Walkthrough extends StatefulWidget {
+  @override
+  WalkthroughState createState() => WalkthroughState();
+}
+
+class WalkthroughState extends State<Walkthrough> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                colors: [
-                  Colors.blue.shade900,
-                  Colors.orange.shade800,
-                  Colors.orange.shade400
-                ]
-            )
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(height: 80,),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  FadeInUp(duration: Duration(milliseconds: 1000), child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 40),)),
-                  SizedBox(height: 10,),
-                  FadeInUp(duration: Duration(milliseconds: 1300), child: Text("Welcome Back", style: TextStyle(color: Colors.white, fontSize: 18),)),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60))
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(30),
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 60,),
-                      FadeInUp(duration: Duration(milliseconds: 1400), child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [BoxShadow(
-                                color: Color.fromRGBO(225, 95, 27, .3),
-                                blurRadius: 20,
-                                offset: Offset(0, 10)
-                            )]
-                        ),
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              padding: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Colors.grey.shade200))
-                              ),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                    hintText: "Email or Phone number",
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Colors.grey.shade200))
-                              ),
-                              child: TextField(
-                                obscureText: true,
-                                decoration: InputDecoration(
-                                    hintText: "Password",
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      )),
-                      SizedBox(height: 40,),
-                      FadeInUp(duration: Duration(milliseconds: 1500), child: Text("Forgot Password?", style: TextStyle(color: Colors.grey),)),
-                      SizedBox(height: 40,),
-                      FadeInUp(duration: Duration(milliseconds: 1600), child: MaterialButton(
-                        onPressed: () {},
-                        height: 50,
-                        // margin: EdgeInsets.symmetric(horizontal: 50),
-                        color: Colors.lightBlue[900],
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50),
-
-                        ),
-                        // decoration: BoxDecoration(
-                        // ),
-                        child: Center(
-                          child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-                        ),
-                      )),
-                      SizedBox(height: 50,),
-                      FadeInUp(duration: Duration(milliseconds: 1700), child: Text("Continue with Sign Up", style: TextStyle(color: Colors.grey),)),
-                      SizedBox(height: 30,),
-                      Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: FadeInUp(duration: Duration(milliseconds: 1800), child: MaterialButton(
-                              onPressed: (){},
-                              height: 50,
-                              color: Colors.blue,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50),
-                              ),
-                              child: Center(
-                                child: Text("Sign Up", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-                              ),
-                            )),
-                          ),
-
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            )
-          ],
+    var screens = IntroScreens(
+      onDone: () => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => loginpage(),
         ),
       ),
+      onSkip: () => print('Skipping the intro slides'),
+      footerBgColor: Colors.blue.withOpacity(.8),
+      activeDotColor: Colors.white,
+      footerRadius: 18.0,
+//      indicatorType: IndicatorType.CIRCLE,
+      slides: [
+        IntroScreen(
+          title: 'Search',
+          //imageAsset: 'assets/img/1.png',
+          description: 'Quickly find all your messages',
+          headerBgColor: Colors.white,
+        ),
+        IntroScreen(
+          title: 'Focused Inbox',
+          headerBgColor: Colors.white,
+          //imageAsset: 'assets/img/2.png',
+          description: "We've put your most important, actionable emails here",
+        ),
+        IntroScreen(
+          title: 'Social',
+          headerBgColor: Colors.white,
+          //imageAsset: 'assets/img/3.png',
+          description: "Keep talking with your mates",
+        ),
+      ],
+    );
+
+    return Scaffold(
+      body: screens,
     );
   }
+}
+
+class NextPage extends StatefulWidget {
+  @override
+  _NextPageState createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  @override
+  Widget build(BuildContext context) => Container(
+    color: Colors.white,
+  );
 }
